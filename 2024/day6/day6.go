9ab4731831b101c01ec3eaff2db4d2903fcce042
Silfadur <no@no.no>
2024-12-06 19:01:24 +0100

    bevor bruteforce

--- 2024/day6/day6.go
+++ 2024/day6/day6.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 )
 
 func check(e error) {
@@ -15,35 +17,38 @@ func check(e error) {
 type Heading int
 
 const (
-	Up = iota
+	None = iota
+	Up
+	Right
 	Down
 	Left
-	Right
 )
 
-type ManufactoringLab struct {
-	guardX       int
-	guardY       int
-	heading      Heading
-	labMap       []string
-	tilesVisited []string
+type Coordinate struct {
+	x int
+	y int
 }
 
-func (ml *ManufactoringLab) run() {
-	for ml.step() {
-		//fmt.Printf("Guard: %d:%d, tile:%s\n", ml.guardX, ml.guardY, string(ml.labMap[ml.guardY][ml.guardX]))
-		ml.updateVisitedTiles()
-	}
-
+type ManufactoringLab struct {
+	guardX            int
+	guardY            int
+	heading           Heading
+	labMap            []string
+	tilesVisited      []string
+	headingGrid       [][]Heading
+	possibleBlockages []Coordinate
 }
 
 func (ml *ManufactoringLab) updateVisitedTiles() {
+	if ml.tilesVisited[ml.guardY][ml.guardX] == '+' {
+		return
+	}
 	if (ml.tilesVisited[ml.guardY][ml.guardX] == '|') ||
-		(ml.tilesVisited[ml.guardY][ml.guardX] == '-') ||
-		(ml.tilesVisited[ml.guardY][ml.guardX] == '+') {
+		(ml.tilesVisited[ml.guardY][ml.guardX] == '-') {
 		ml.tilesVisited[ml.guardY] = ml.tilesVisited[ml.guardY][:ml.guardX] + "+" + ml.tilesVisited[ml.guardY][ml.guardX+1:]
 		return
 	}
+	ml.headingGrid[ml.guardY][ml.guardX] = ml.heading
 	switch ml.heading {
 	case Up, Down:
 		ml.tilesVisited[ml.guardY] = ml.tilesVisited[ml.guardY][:ml.guardX] + "|" + ml.tilesVisited[ml.guardY][ml.guardX+1:]
@@ -116,6 +121,124 @@ func (ml *ManufactoringLab) countVisitedTiles() int {
 	return visited
 }
 
+func (ml *ManufactoringLab) findPossibleBlockages() int {
+	var blockages int
+	for y, line := range ml.tilesVisited {
+		for x, symbol := range line {
+			if symbol == '+' {
+				switch ml.headingGrid[y][x] {
+				case Up:
+					if ml.tilesVisited[y-1][x] != '#' && ml.headingGrid[y][x-1] == Left {
+						//fmt.Printf("Possible Blockage, crossing reroute: %d:%d\n", x-1, y)
+						ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x - 1, y: y})
+						blockages++
+					} else if ml.tilesVisited[y-1][x] == '#' {
+						searchY := y
+						cancel := false
+						for !cancel {
+							searchY++
+							if searchY < len(ml.labMap) {
+								switch ml.tilesVisited[searchY][x] {
+								case '-':
+									if ml.headingGrid[searchY][x] == Left {
+										//fmt.Printf("Possible Blockage, straight reroute: %d:%d\n", x-1, searchY)
+										ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x - 1, y: searchY})
+										blockages++
+									}
+								case '#':
+									cancel = true
+								}
+							} else {
+								cancel = true
+							}
+						}
+					}
+				case Right:
+					if ml.tilesVisited[y][x+1] != '#' && ml.headingGrid[y-1][x] == Up {
+						//fmt.Printf("Possible Blockage, crossing reroute: %d:%d\n", x, y-1)
+						ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x, y: y - 1})
+						blockages++
+					} else if ml.tilesVisited[y][x+1] == '#' {
+						searchX := x
+						cancel := false
+						for !cancel {
+							searchX--
+							if searchX >= 0 {
+								switch ml.tilesVisited[y][searchX] {
+								case '|':
+									if ml.headingGrid[y][searchX] == Up {
+										//fmt.Printf("Possible Blockage, straight reroute: %d:%d\n", searchX, y-1)
+										ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: searchX, y: y - 1})
+										blockages++
+									}
+								case '#':
+									cancel = true
+								}
+							} else {
+								cancel = true
+							}
+						}
+					}
+				case Down:
+					if ml.tilesVisited[y+1][x] != '#' && ml.headingGrid[y][x+1] == Right {
+						//fmt.Printf("Possible Blockage, crossing reroute: %d:%d\n", x+1, y)
+						ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x + 1, y: y})
+						blockages++
+					} else if ml.tilesVisited[y+1][x] == '#' {
+						searchY := y
+						cancel := false
+						for !cancel {
+							searchY--
+							if searchY >= 0 {
+								//fmt.Printf("searching: %d:%d, %s\n", x, searchY, string(ml.tilesVisited[searchY][x]))
+								switch ml.tilesVisited[searchY][x] {
+								case '-':
+									if ml.headingGrid[searchY][x] == Right {
+										//fmt.Printf("Possible Blockage, straight reroute: %d:%d\n", x+1, searchY)
+										ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x + 1, y: searchY})
+										blockages++
+									}
+								case '#':
+									cancel = true
+								}
+							} else {
+								cancel = true
+							}
+						}
+					}
+				case Left:
+					if ml.tilesVisited[y][x-1] != '#' && ml.headingGrid[y+1][x] == Down {
+						//fmt.Printf("Possible Blockage, crossing reroute: %d:%d\n", x, y+1)
+						ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: x, y: y + 1})
+						blockages++
+					} else if ml.tilesVisited[y][x-1] == '#' {
+						searchX := x
+						cancel := false
+						for !cancel {
+							searchX++
+							if searchX < len(ml.labMap[y]) {
+								switch ml.tilesVisited[y][searchX] {
+								case '|':
+									if ml.headingGrid[y][searchX] == Down {
+										//fmt.Printf("Possible Blockage, straight reroute: %d:%d\n", searchX, y+1)
+										ml.possibleBlockages = append(ml.possibleBlockages, Coordinate{x: searchX, y: y + 1})
+										blockages++
+									}
+								case '#':
+									cancel = true
+								}
+							} else {
+								cancel = true
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return blockages
+}
+
 func (ml *ManufactoringLab) loadMap(filename string) {
 	dat, err := os.Open(filename)
 	check(err)
@@ -131,6 +254,10 @@ func (ml *ManufactoringLab) loadMap(filename string) {
 			newLine += "."
 		}
 		ml.tilesVisited[i] = newLine
+	}
+	ml.headingGrid = make([][]Heading, len(ml.labMap))
+	for i := 0; i < len(ml.labMap); i++ {
+		ml.headingGrid[i] = make([]Heading, len(ml.labMap[i]))
 	}
 	ml.findGuard()
 	ml.updateVisitedTiles()
@@ -167,19 +294,67 @@ func (ml *ManufactoringLab) findGuard() {
 			}
 		}
 	}
-	fmt.Printf("Guard: %d:%d, tile:%s\n", ml.guardX, ml.guardY, string(ml.labMap[ml.guardY][ml.guardX]))
+	//fmt.Printf("Guard: %d:%d, tile:%s\n", ml.guardX, ml.guardY, string(ml.labMap[ml.guardY][ml.guardX]))
 }
 
 func printMap(input []string) {
-	for _, line := range input {
+	fmt.Printf("\\ ")
+	for i, _ := range input[0] {
+		fmt.Printf("%d", i)
+	}
+	fmt.Println("")
+	for i, line := range input {
+		fmt.Println(i, line)
+	}
+}
+func (ml *ManufactoringLab) printHeadings() {
+	for _, line := range ml.headingGrid {
 		fmt.Println(line)
 	}
 }
 
+func (ml *ManufactoringLab) run(done chan bool) {
+	for ml.step() {
+		//fmt.Printf("Guard: %d:%d, tile:%s\n", ml.guardX, ml.guardY, string(ml.labMap[ml.guardY][ml.guardX]))
+		ml.updateVisitedTiles()
+	}
+	done <- true
+}
 func main() {
 	var lab ManufactoringLab
-	lab.loadMap("input.txt")
-	lab.run()
+	testfile := "input.txt"
+	lab.loadMap(testfile)
+	explorationDone := make(chan bool, 1)
+	lab.run(explorationDone)
 	printMap(lab.tilesVisited)
-	fmt.Println("Tiles visited:", lab.countVisitedTiles())
+	fmt.Println("Possible blockages found:", lab.findPossibleBlockages())
+	var verifiedBlockage int
+	var failedBlockages int
+	var wg sync.WaitGroup
+	for _, possibleBlockage := range lab.possibleBlockages {
+		wg.Add(1)
+		worker := func() {
+			fmt.Println("Testing Blockage:", possibleBlockage)
+
+			var testlab ManufactoringLab
+			testlab.loadMap(testfile)
+
+			testlab.labMap[possibleBlockage.y] = testlab.labMap[possibleBlockage.y][:possibleBlockage.x] + "#" + testlab.labMap[possibleBlockage.y][possibleBlockage.x+1:]
+
+			noLoop := make(chan bool, 1)
+			go testlab.run(noLoop)
+			select {
+			case <-noLoop:
+				failedBlockages++
+				wg.Done()
+			case <-time.After(1 * time.Second):
+				verifiedBlockage++
+				wg.Done()
+			}
+		}
+		go worker()
+	}
+	wg.Wait()
+	fmt.Println("VerifiedBlockages:", verifiedBlockage)
+	fmt.Println("FailedBlockages:", failedBlockages)
 }
