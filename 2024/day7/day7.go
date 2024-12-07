646b25ce7cc94b4e1c551fe630766b689f6abb38
Silfadur <no@no.no>
2024-12-07 17:31:53 +0100

    day7 part 1

---
+++ 2024/day7/day7.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Calibration struct {
+	testValue int
+	numbers   []int
+}
+
+func readFile(filename string) []Calibration {
+	var cals []Calibration
+	file, err := os.Open(filename)
+	check(err)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+		var cal Calibration
+		cal.testValue, err = strconv.Atoi(parts[0])
+		check(err)
+		numbers := strings.Split(parts[1], " ")
+		for _, number := range numbers {
+			parsed, err := strconv.Atoi(number)
+			if err != nil {
+				continue
+			}
+			cal.numbers = append(cal.numbers, parsed)
+		}
+		cals = append(cals, cal)
+	}
+	return cals
+}
+
+func deepCopy(original Calibration) Calibration {
+	newSlice := make([]int, len(original.numbers))
+	copy(newSlice, original.numbers)
+	original.numbers = newSlice
+	return original
+}
+
+func verifyCalibrationRec(cal Calibration, currentValue int) bool {
+	if currentValue > cal.testValue {
+		return false
+	}
+	nextValue := cal.numbers[0]
+	addResult := currentValue + nextValue
+	mulResult := currentValue * nextValue
+	if len(cal.numbers) == 1 && (addResult == cal.testValue || mulResult == cal.testValue) {
+		return true
+	} else if len(cal.numbers) == 1 {
+		return false
+	} else {
+		cal.numbers = cal.numbers[1:]
+		return verifyCalibrationRec(deepCopy(cal), addResult) || verifyCalibrationRec(deepCopy(cal), mulResult)
+	}
+}
+func verifyCalibration(cal Calibration, counter *atomic.Uint64) {
+	copy := deepCopy(cal)
+	currentValue := copy.numbers[0]
+	copy.numbers = copy.numbers[1:]
+	if verifyCalibrationRec(copy, currentValue) {
+		counter.Add(uint64(cal.testValue))
+	}
+}
+
+func main() {
+	calibrations := readFile("input.txt")
+	var wg sync.WaitGroup
+	var counter atomic.Uint64
+	for _, cal := range calibrations {
+		wg.Add(1)
+		go func() {
+			verifyCalibration(cal, &counter)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Result:", counter.Load())
+
+}
